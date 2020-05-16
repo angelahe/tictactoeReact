d6c0149886226f3c8cf0daaa9b407310e0835e0c
angela <angela@aviak.ca>
2020-05-16 10:29:48 -0600

    render status to show who has the next turn

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ class Board extends React.Component {
             xIsNext: !this.state.xIsNext,
         });
     }
-    
+
     renderSquare(i) {
       return (
         <Square 
@@ -38,7 +38,7 @@ class Board extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
